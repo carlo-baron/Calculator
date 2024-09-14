c65e53f880cf447a86973f7be6af30a5f4f7858b
carlo-baron <carlobaron430@gmail.com>
2024-09-14 21:58:16 +0800

    I was trying to squeeze every remaining brain cells and apparently you can use eval to cheat the system. Ok

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const inputField = document.getElementById("input-field");
 const buttons = document.getElementsByClassName("buttons");
 const buttonsArray = Array.from(buttons);
 
+
 buttonsArray.forEach(element => {
     element.addEventListener("click", clickEvent)
 });
@@ -9,13 +10,20 @@ buttonsArray.forEach(element => {
 function clickEvent(event){
     const value = event.target.value;
     if(value == "="){
-        console.log("Equal")
+        console.log("Equal");
+        console.log(inputField.value);
+        equalFunction();
     }else if(value == "C"){
+        // inputField.value = inputField.value.slice(0, -1);
         inputField.value = "";
     }else{
         inputField.value += value;
         inputField.scrollLeft = inputField.scrollWidth;
     }
-
 }
 
+function equalFunction(){
+    const pattern = /[+\-*\/]/g
+    console.log(pattern.test(inputField.value));
+    console.log(eval(inputField.value)); // evalue is cheating what the heal
+}
