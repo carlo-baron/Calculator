e84f81be218b83b7eaf7c6e67abd7bf0432e2027
carlo-baron <carlobaron430@gmail.com>
2024-09-14 10:07:58 +0800

    Javascript start

---
+++ index.js
@@ -0,0 +1,20 @@
+const inputField = document.getElementById("input-field");
+const buttons = document.getElementsByClassName("buttons");
+const buttonsArray = Array.from(buttons);
+
+buttonsArray.forEach(element => {
+    element.addEventListener("click", clickEvent)
+});
+
+function clickEvent(event){
+    const value = event.target.value;
+    if(value == "="){
+        console.log("Equal")
+    }else if(value == "C"){
+        inputField.value = "";
+    }else{
+        inputField.value += value;
+    }
+
+}
+
